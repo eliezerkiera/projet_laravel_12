e88896269c6b0aadf80023fef6f546e565c8202a
Eliezer KIERA <kieraeliezer7@yahoo.fr>
2025-09-02 22:14:35 +0000

    Create biddirectives

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\View\Directives\BindDirective;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -24,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
         //
 
         Schema::defaultStringLength(191);
+
+        BindDirective::register();
     }
 }
---
+++ app/View/Directives/BindDirective.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\View\Directives;
+
+use Illuminate\Support\Facades\Blade;
+
+class BindDirective
+{
+    public static function register()
+    {
+        // Directive ouvrante
+        Blade::directive('bind', function ($expression) {
+            return "<?php \$__bindData = {$expression}; ?>";
+        });
+
+        // Directive fermante
+        Blade::directive('endbind', function () {
+            return "<?php unset(\$__bindData); ?>";
+        });
+
+        // Directive pour remplir les champs
+        Blade::directive('field', function ($expression) {
+            return "<?php echo e(old({$expression}, \$__bindData->{$expression} ?? '')); ?>";
+        });
+    }
+}
