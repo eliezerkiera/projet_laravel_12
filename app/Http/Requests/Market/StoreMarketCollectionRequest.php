b617e7b5751aa7f92b63f733eaf5627465b986de
Eliezer KIERA <kieraeliezer7@yahoo.fr>
2025-09-28 16:12:48 +0000

    Create market form requests

---
+++ app/Http/Requests/Market/StoreMarketCollectionRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Market;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreMarketCollectionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'user_id' => 'required|exists:users,id',
+            'country_id' => 'required|exists:countries,id',
+            'market_collection_type_id' => 'required|exists:market_collection_types,id',
+            'market_collection_category_id' => 'required|exists:market_collection_categories,id',
+        ];
+    }
+}
---
+++ app/Http/Requests/Market/StoreMarketProductRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Market;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreMarketProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+            'working_state' => 'nullable|numeric|min:0|max:100',
+            'user_id' => 'required|exists:users,id',
+            'country_id' => 'required|exists:countries,id',
+            'market_product_type_id' => 'required|exists:market_product_types,id',
+            'market_product_category_id' => 'required|exists:market_product_categories,id',
+            'market_product_payment_method_id' => 'required|exists:market_product_payment_methods,id',
+            'currency_id' => 'required|exists:currencies,id',
+        ];
+    }
+}
